47ca5e548e1e2253361d0fa4325482af205d11fe
MamiCiga <113908740+MamiCiga@users.noreply.github.com>
2022-09-19 15:51:11 +0300

    Create odev1-1.sql

---
+++ odev1-1.sql
@@ -0,0 +1,7 @@
+select * from film
+where length > 60 and length < 75
+where rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99
+select * from customer
+where first_name = 'Mary'
+SELECT * from film
+WHERE rental_rate <> 2.99 and rental_rate <> 4.99 and length < 50
