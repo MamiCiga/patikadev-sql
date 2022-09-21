f85aafe1cf6448df3152accf9d255c4755a36273
MamiCiga <113908740+MamiCiga@users.noreply.github.com>
2022-09-21 15:14:04 +0300

    Create odev3-3.sql

---
+++ odev3-3.sql
@@ -0,0 +1,8 @@
+SELECT *FROM country
+where country LIKE 'A%'
+SELECT *FROM country
+WHERE country LIKE '_____%n'
+SELECT * FROM film
+WHERE title ILIKE '%T%T%T%T%'
+SELECT *FROM film
+Where title LIKE 'C%' AND length > 90 AND rental_rate =2.99
