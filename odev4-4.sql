3e2d2d778d2d63f0ba3c26390beeafcacd723ac2
MamiCiga <113908740+MamiCiga@users.noreply.github.com>
2022-09-22 10:43:10 +0300

    Create odev4-4.sql

---
+++ odev4-4.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT replacement_cost FROM film
+SELECT COUNT (DISTINCT replacement_cost) FROM film
+Select COUNT (title) from film
+where title LIKE 'T%'AND rating = 'G'
+Select COUNT (country '_____') from country
+SELECT COUNT (*) FROM city
+WHERE city LIKE 'R%'
