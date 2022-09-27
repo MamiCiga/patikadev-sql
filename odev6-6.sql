6f2e1e2153bc1aa09c7a8f8d68a39d9e4b4f3e8c
MamiCiga <113908740+MamiCiga@users.noreply.github.com>
2022-09-27 12:04:57 +0300

    Create odev6-6.sql

---
+++ odev6-6.sql
@@ -0,0 +1,7 @@
+SELECT AVG(rental_rate) FROM film
+SELECT COUNT (*) FROM film
+WHERE title LIKE 'C%'
+SELECT MAX(length) from film
+Where rental_rate =0.99
+SELECT COUNT (DISTINCT replacement_cost) from film
+Where length > 150
